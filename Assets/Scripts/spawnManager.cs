83f6ab137e25f4911a2706a591ac0e59404c987d
zabayrer <122053629+zabayrer@users.noreply.github.com>
2023-03-21 11:21:19 -0700

    fixed the issue with 4.4.3

--- Assets/Scripts/spawnManager.cs
+++ Assets/Scripts/spawnManager.cs
@@ -16,7 +16,7 @@ public class spawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //enemyCount = FindObjectsOfType<Enemy>().Length;
+        enemyCount = FindObjectsOfType<enemyScript>().Length;
 
         if (enemyCount == 0)
         {
